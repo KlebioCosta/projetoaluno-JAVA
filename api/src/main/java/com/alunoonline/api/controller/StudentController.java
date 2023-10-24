9b5a5c3f0ef7de712200d10d816f607d89870b6e
Edinailton Klébio C  Ferreira <112285772+KlebioCosta@users.noreply.github.com>
2023-10-24 18:13:53 -0300

    ..

---
+++ api/src/main/java/com/alunoonline/api/controller/StudentController.java
@@ -0,0 +1,38 @@
+package com.alunoonline.api.controller;
+
+import com.alunoonline.api.model.Student;
+import com.alunoonline.api.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    StudentService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Student> create (@RequestBody Student student) {
+        Student studentCreated = service.create(student);
+
+        return ResponseEntity.status(201).body(studentCreated);
+    }
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Student> findAll() {
+        return service.findAll();
+    }
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Student> findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+} 
---
+++ api/src/main/java/com/alunoonline/api/service/StudentService.java
@@ -0,0 +1,27 @@
+package com.alunoonline.api.service;
+
+import com.alunoonline.api.model.Student;
+import com.alunoonline.api.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    StudentRepository repository;
+
+    public Student create(Student student) {
+        return repository.save(student);
+    }
+    public List<Student> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Student> findById(Long id) {
+        return repository.findById(id);
+    }
+}
